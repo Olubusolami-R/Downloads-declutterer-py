1e71cb00a5d450ee1706680d2502fcea758d157e
Olubusolami Sogunle <busolasogunle@gmail.com>
2024-01-23 13:31:16 +0100

    Add folder names

---
+++ script.py
@@ -0,0 +1,14 @@
+folder_names = {
+    "Audio": {'aif','cda','mid','midi','mp3','mpa','ogg','wav','wma'},
+    "Compressed":{'7z','deb','pkg','rar','rpm', 'tar.gz','z', 'zip'},
+    'Code':{'js','jsp','html','ipynb','py','c','css', 'ts','tsx'},
+    'Other Documents':{'ppt','pptx','xls', 'xlsx','doc','docx','txt', 'tex'},
+    'Epubs':{'epub'},
+    'PDFs':{'pdf'},
+    'Images':{'bmp','gif','ico','jpeg','jpg','png','jfif','svg','tif','tiff','heic','webp'},
+    'Software':{'apk','bat','bin', 'exe','jar','msi','dmg'},
+    'Videos':{'3gp','avi','flv','h264','mkv','mov','mp4','mpg','mpeg','wmv'},
+    'Others': {'NONE'}
+}
+
+
