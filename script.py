21112b7afeedcce7d76e7420076bec6eaa7b2e47
Olubusolami Sogunle <busolasogunle@gmail.com>
2024-01-24 13:09:24 +0100

    Add steps 6 and 7

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ folder_names = {
     'Epubs':{'epub'},
     'PDFs':{'pdf'},
     'Images':{'bmp','gif','ico','jpeg','jpg','png','jfif','svg','tif','tiff','heic','webp'},
-    'Other Documents':{'ppt','pptx','xls', 'xlsx','doc','docx','txt', 'tex'},
+    'Other_Documents':{'ppt','pptx','xls', 'xlsx','doc','docx','txt', 'tex'},
     'Software':{'apk','bat','bin', 'exe','jar','msi','dmg'},
     'Videos':{'3gp','avi','flv','h264','mkv','mov','mp4','mpg','mpeg','wmv'},
     'Others': {'NONE'}
@@ -38,8 +38,16 @@ all_folders=[os.path.join(downloads_folder_path,f) for f in os.listdir(downloads
 print(len(all_folders))
 
 #Step 6: Create the new and organised folder paths
-#Step 7: Create folders from paths
-#Step 8: Create function that creates new paths for files 
+new_folder_paths=[os.path.join(downloads_folder_path, folder) for folder in folder_names]
+print(new_folder_paths)
+
+#Step 7: Create new folders from created paths
+for new_folder in new_folder_paths:
+    os.mkdir(new_folder)
+
+#Step 8: Create a tweaker function that creates new paths for files 
+
+
 #Step 9: Use the function to rename al; the files from 4
 #Step 10: Move existing folders to into the newly created "Others" folder.
 
